4479a35292194355617e28b86eafffb8b4ed41c3
Rahmattrik <rahmattrik@gmail.com>
2024-05-13 15:59:46 +0700

    feat: add login

--- apps/web/src/components/Navbar.tsx
+++ apps/web/src/components/Navbar.tsx
@@ -6,9 +6,18 @@ import React from 'react';
 import { Button } from './ui/button';
 import { Badge } from './ui/badge';
 import { useRouter } from 'next/navigation';
+import { useAppDispatch, useAppSelector } from '@/redux/hooks';
+import { logoutAction } from '@/redux/slice/userSlice';
 
 const Navbar = () => {
+  const dispatch = useAppDispatch();
+  const { id } = useAppSelector((state) => state.user);
   const router = useRouter();
+
+  const logout = () => {
+    localStorage.removeItem('token');
+    dispatch(logoutAction());
+  }
   return (
     <>
       <nav className=" text-whites bg-woodsmokey ">
@@ -27,7 +36,34 @@ const Navbar = () => {
               </Link>
             </div>
 
+            {Boolean(id) ? (
             <div className="flex items-center gap-8 ml-5">
+              <Link href={'/events'}>
+                <h3 className="events">Events</h3>
+              </Link>
+              <Link href={'/venues'}>
+                <h3 className="venue">Venues</h3>
+              </Link>
+              <Link href={'/about'}>
+                <h3 className="about">About</h3>
+              </Link>
+              <Link href={'/contact'}>
+                <h3 className="contact">Contact</h3>
+              </Link>
+              <div>
+                <Badge className="bg-brown-shades rounded-s-full p-2 hover:bg-brown-tints"
+                  onClick={logout}
+                >
+                  <Link href={'/login'}>Logout</Link>
+                </Badge>
+                <Badge className="bg-brown-shades rounded-e-full p-2 hover:bg-brown-tints" 
+                  onClick={() => router.push('/register')}>
+                  <h5>Register</h5>
+                </Badge>
+              </div>
+            </div>
+            ) : (
+              <div className="flex items-center gap-8 ml-5">
               <Link href={'/events'}>
                 <h3 className="events">Events</h3>
               </Link>
@@ -49,6 +85,8 @@ const Navbar = () => {
                 </Badge>
               </div>
             </div>
+            )}
+            
           </div>
         </div>
       </nav>
